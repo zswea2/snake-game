a82d7d9b3a394a586dff04c93f0202f269b81d25
inhwan You <zswea12@gmail.com>
2022-01-16 02:43:35 +0900

    3 commit

--- snake game.py
+++ snake game.py
@@ -6,10 +6,12 @@ import random
 pygame.init()
 #전역변수
 WHITE = (255,255,255)
-size = [600,500]
+size = [700,600]  #600,500
 screen = pygame.display.set_mode(size)
 RED = (255,0,0)
 GREEN = (0,255,0)
+score = 0
+font = pygame.font.Font(None,30)
 
 done = False
 clock = pygame.time.Clock()
@@ -23,6 +25,10 @@ KEY_DIRECTION = {
     pygame.K_RIGHT: 'E',
 }
 
+#점수판
+def display_score():
+    txt_score = font.render('Score : {}'.format(score),True,(0,0,0,))
+    screen.blit(txt_score,(0,0))
 
 def draw_block(screen, color, position):
     block = pygame.Rect((position[0]*20,position[1]*20),(20,20))
@@ -31,7 +37,7 @@ def draw_block(screen, color, position):
 #클래스
 class Snake:
     def __init__(self):
-        self.positions = [(2,0),(1,0),(0,0)] #뱀의 위치, 2,0이 머리이다
+        self.positions = [(2,3),(1,3),(0,3)] #뱀의 위치, 2,0이 머리이다
         self.direction = ''
 
     def draw(self):
@@ -68,6 +74,7 @@ class Snake:
             self.positions.append((x - 1, y))
         elif self.direction == 'E':
             self.positions.append((x + 1, y))
+        
 
 class Apple:
     def __init__(self, position = (5,5)):
@@ -76,14 +83,20 @@ class Apple:
     def draw(self):
         draw_block(screen,RED,self.position)
 
+def game_over():
+    gameover_font  = pygame.font.Font(None, 60)
+    txt_game_over = gameover_font.render("game over",True,(0,0,0))
+    game_reat = txt_game_over.get_rect(center = (300,250))
+    screen.blit(txt_game_over,game_reat)
 
 
 #루프
 def runGame():
-    global done, last_moved_time, moving_direction
+    global done, last_moved_time,score
     #게임 시작시 뱀과 사과를 초기화
     snake = Snake()
     apple = Apple()
+
 
     while not done:
         clock.tick(10)
@@ -100,12 +113,22 @@ def runGame():
             snake.move()
         if snake.positions[0] == apple.position:
             snake.grow()
-            apple.position = (random.randint(0,30),random.randint(0,25))
+            apple.position = (random.randint(0,34),random.randint(2,29))
             # print("apple!:",apple.position)
+            score = score + 1 
         #꼬리에 닿았을때 종료
         if snake.positions[0] in snake.positions[1:]:
+            game_over()
+            pygame.display.update()
+            pygame.time.delay(2000)
             done = True
+        
+        
+        pygame.draw.rect(screen,(192,192,192),(0,0,700,40))
 
+        
+        
+        display_score()
         snake.draw()
         apple.draw()
         pygame.display.update()
